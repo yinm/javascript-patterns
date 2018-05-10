aa6b896efd192746c985bc13f56b4405a488ccde
yinm <yinm.cminor9@gmail.com>
2018-05-10 22:51:48 +0900

    Check deep copy

--- ch6/sample.js
+++ ch6/sample.js
@@ -1,8 +1,18 @@
-function extend(parent, child) {
+function extendDeep(parent, child) {
+  const
+    toStr = Object.prototype.toString,
+    astr = '[object Array]'
+
   child = child || {}
+
   for (let i in parent) {
     if (parent.hasOwnProperty(i)) {
-      child[i] = parent[i]
+      if (typeof parent[i] === 'object') {
+        child[i] = (toStr.call(parent[i]) === astr) ? [] : {}
+        extendDeep(parent[i], child[i])
+      } else {
+        child[i] = parent[i]
+      }
     }
   }
 
@@ -13,8 +23,14 @@ const dad = {
   counts: [1, 2, 3],
   reads: {paper: true}
 }
+let kid = extendDeep(dad)
 
-let kid = extend(dad)
 kid.counts.push(4)
+console.log(kid.counts.toString())
 console.log(dad.counts.toString())
+
+console.log(kid.reads.paper)
 console.log(dad.reads === kid.reads)
+kid.reads.paper = false
+kid.reads.web = true
+console.log(dad.reads.paper)
